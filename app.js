40f2771b88b72e07f7bf8bc296bde6de9cd40fbf
Arjun Santhosh <arjunsanthosh738@gmail.com>
2025-01-06 20:27:08 +0530

    first commit

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use(helmet({
 
 // Configure CORS
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://upskilljapan.netlify.app', 'https://japanese-lms-features-test.netlify.app'],
+    origin: ['http://localhost:5173', 'https://upskilljapan.netlify.app', 'https://japanese-lms-features-test.netlify.app', 'https://learnese-beta.netlify.app'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
